3f53402bbfd4a1a1fc578b17031eaa367178f7f8
sbm-landrade <lucas.andrade@sbmtechnology.com.br>
2024-06-20 20:40:10 -0300

    Criando a tabela pelo assistente

---
+++ tbproduto.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `sucos`.`tbproduto` (
+  `PRODUTO` varchar(20) NULL,
+  `NOME` varchar(150) NULL,
+  `EMBALAGEM` varchar(50) NULL,
+  `TAMANHO` varchar(50) NULL,
+  `SABOR` varchar(50) NULL,
+  `PRECO_LISTA` float NULL
+)
